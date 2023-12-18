46acc61a2698ad3ce091867f7c09b4aa40d9b62d
awesomemax <cvb.cvb.2017.cvb@gmail.com>
2023-12-19 01:09:25 +0300

    method not allowed

--- src/main/java/com/example/nothingbuttext/dto/YandexGpt/YandexGptGenerationOptions.java
+++ src/main/java/com/example/nothingbuttext/dto/YandexGpt/YandexGptGenerationOptions.java
@@ -5,13 +5,38 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-public class YandexGptGenerationOptions {
-    private boolean partialResults;
+import java.io.Serializable;
 
+public class YandexGptGenerationOptions {
     public YandexGptGenerationOptions(boolean partialResults, double temperature, int maxTokens) {
         this.partialResults = partialResults;
         this.temperature = temperature;
         this.maxTokens = maxTokens;
+    }
+    private boolean partialResults;
+
+    public void setPartialResults(boolean partialResults) {
+        this.partialResults = partialResults;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public void setMaxTokens(int maxTokens) {
+        this.maxTokens = maxTokens;
+    }
+
+    public boolean isPartialResults() {
+        return partialResults;
+    }
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public int getMaxTokens() {
+        return maxTokens;
     }
 
     private double temperature; //Should be a double number between 0 (inclusive) and 1 (inclusive)
